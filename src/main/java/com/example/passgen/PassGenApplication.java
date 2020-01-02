dce37dcec260fdde96ae6e7ebc008c71410a77b9
Denis100195 <denfil1995@gmail.com>
2020-01-02 12:11:02 +0300

    commit

---
+++ src/main/java/com/example/passgen/PassGenApplication.java
@@ -0,0 +1,12 @@
+package com.example.passgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassGenApplication.class, args);
+    }
+}
